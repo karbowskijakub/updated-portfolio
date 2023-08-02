9cb9566254bc97b191b5724f8ec24e59befd5a93
karbzy <jakub.karbowski3@gmail.com>
2023-08-02 21:13:23 +0200

    fix next.config.js v2

--- next.config.js
+++ next.config.js
@@ -15,7 +15,6 @@ module.exports = {
       // Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
         resourceQuery: { not: /url/ }, // exclude if *.svg?url
         use: ['@svgr/webpack'],
       },
